2a919d81b81a4e3736e337c3c50b4fd48b31de4b
ACKA KOUDOU ARMEL <africonnexion.com@gmail.comm>
2025-02-27 16:46:22 +0000

    Inittialisation du prjet

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import { APP_DESCRIPTION, APP_NAME, APP_SLOGAN } from "@/lib/constants";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: `${APP_NAME}. ${APP_SLOGAN}`,
+  },
+  description: APP_DESCRIPTION,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`min-h-screen ${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
