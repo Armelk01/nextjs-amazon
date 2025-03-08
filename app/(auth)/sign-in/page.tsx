14428e1b93d4165e49d2399fc94994ea9370a976
ACKA KOUDOU ARMEL <africonnexion.com@gmail.comm>
2025-03-08 17:54:40 +0000

    se connecter via son compte google

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -10,6 +10,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
 import CredentialsSignInForm from './credentials-signin-form'
+import { GoogleSignInForm } from './google-signin-form'
 
 
 export const metadata: Metadata = {
@@ -40,6 +41,10 @@ export default async function SignIn(props: {
           <div>
             <CredentialsSignInForm/>
           </div>
+          <SeparatorWithOr />
+            <div className='mt-4'>
+              <GoogleSignInForm />
+            </div>
         </CardContent>
       </Card>
       <SeparatorWithOr>New to {APP_NAME}?</SeparatorWithOr>
