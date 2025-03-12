91a21cd968ec774276129717b58e862d5398a360
ACKA KOUDOU ARMEL <africonnexion.com@gmail.comm>
2025-03-12 23:18:53 +0000

    26-add-theme-color

--- components/shared/client-providers.tsx
+++ components/shared/client-providers.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import useCartSidebar from '@/hooks/use-cart-sidebar'
 import CartSidebar from './cart-sidebar'
 import { Toaster } from '../ui/sonner'
+import { ThemeProvider } from './theme-provider'
 
 
 export default function ClientProviders({
@@ -13,16 +14,16 @@ export default function ClientProviders({
   const isCartSidebarOpen = useCartSidebar()
 
   return (
-    <>
-      {isCartSidebarOpen ? (
-        <div className='flex min-h-screen'>
-          <div className='flex-1 overflow-hidden'>{children}</div>
-          <CartSidebar />
-        </div>
-      ) : (
-        <div>{children}</div>
-      )}
-      <Toaster />
-    </>
+    <ThemeProvider attribute='class' defaultTheme='system'>
+        {isCartSidebarOpen ? (
+          <div className='flex min-h-screen'>
+            <div className='flex-1 overflow-hidden'>{children}</div>
+            <CartSidebar />
+          </div>
+        ) : (
+          <div>{children}</div>
+        )}
+        <Toaster />
+      </ThemeProvider>
   )
 }
