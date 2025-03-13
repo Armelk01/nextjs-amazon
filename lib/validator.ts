52e322abf952463d444f48cb04d2ab61de1c5620
ACKA KOUDOU ARMEL <africonnexion.com@gmail.comm>
2025-03-13 16:50:14 +0000

    29-create-update-products

--- lib/validator.ts
+++ lib/validator.ts
@@ -199,3 +199,6 @@ export const UserSignUpSchema = UserSignInSchema.extend({
 export const UserNameSchema = z.object({
   name: UserName,
 })
+export const ProductUpdateSchema = ProductInputSchema.extend({
+  _id: z.string(),
+})
